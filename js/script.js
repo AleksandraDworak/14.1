45689d751f9f8e9ae7a65592850de0fa6d0a1884
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-01-29 20:59:41 +0100

    14.4 v1:wq

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ results.insertAdjacentHTML('beforeend', items);
 var flkty = new Flickity('.carousel');
 var restartButton = document.querySelector('.restart');
 restartButton.addEventListener( 'click', function() {
-	flkty.selectCell( 0, true, true )
+	flkty.selectCell( 0, true, true );
 });
 
 var progressBar = document.querySelector('.progress-bar')
@@ -26,15 +26,29 @@ flkty.on( 'scroll', function( progress ) {
 window.initMap = function() {
     var firstSlide = {lat: 52.408086, lng: 16.905217};
     var map = new google.maps.Map(
-    	document.getElementById('map'), {zoom: 12, center: firstSlide});
-	var marker = new google.maps.Marker({position: firstSlide, map: map});
-
-	for(var i = 0; i < cellData.length; i++){
+    	document.getElementById('map'), {zoom: 10, center: firstSlide});
+	var marker1 = new google.maps.Marker({position: firstSlide, map: map});
+	
+	cellData.forEach(function(currentValue, index){
+		//console.log(index);
+		//console.log(currentValue);
 		var marker = new google.maps.Marker({
-			position: {lat:Number(cellData[i].coords.lat), lng: Number(cellData[i].coords.lng)}, 
+			position: {lat:Number(cellData[index].coords.lat), lng: Number(cellData[index].coords.lng)}, 
 			map: map
+		})
+		marker.addListener('click', function() {
+		flkty.selectCell(index, true, true );
 		});
-	};
-}
+	});	
+
+	flkty.on( 'change', function( index ) {
+	map.setCenter({
+		lat : Number(cellData[index].coords.lat),
+		lng : Number(cellData[index].coords.lng)
+	});
+});
+};
+
 
 })();
+//cellData.forEach` i zamiast `i` użyj `index` z argumentów callbacka `forEach
